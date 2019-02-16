b3e1038714487a5bc3bafbb98c281925fe52b67d
abdullah temel <Aura08@windowslive.com>
2019-02-16 19:54:45 +0300

    first

--- ARKCRM/Controllers/HomeController.cs
+++ ARKCRM/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ARKCRM.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
-        }
+        } 
 
         public ActionResult Contact()
         {
