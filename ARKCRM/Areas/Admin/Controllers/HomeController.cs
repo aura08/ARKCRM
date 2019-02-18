ea16b3aea8e3ffa8e129b38163138c4a4280a53b
abdullah temel <Aura08@windowslive.com>
2019-02-18 18:05:19 +0300

    Admin bar,and page create,all issues accomplished

--- ARKCRM/Areas/Admin/Controllers/HomeController.cs
+++ ARKCRM/Areas/Admin/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ARKCRM.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,19 @@ namespace ARKCRM.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Admin/Home
+        ArkcrmContext db = new ArkcrmContext();
         public ActionResult Index()
         {
+
+            return View();
+        }
+      
+        public ActionResult PartialIndex(string tableName) 
+        {
+             
+
+
+
             return View();
         }
     }
